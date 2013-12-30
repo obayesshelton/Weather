5a6f0d8cec05b542bc21c39ded82ee79404675bf
Oliver Bayes-Shelton <me@bayes-shelton.co.uk>
2013-12-30 19:37:22 +0000

    basics of old work

---
+++ src/Weather/Classes/APICalls.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Weather\Classes;
+
+class APICalls
+{
+	public $host;
+
+	public $protocol;
+
+	public $type;
+
+	public $path;
+
+	public function test() {
+		$foo = 'foo';
+		return $foo;
+	}
+}
